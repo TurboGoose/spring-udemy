2116591eefc3b69aa938cb1d7941c76996fcc063
TurboGoose <antonpuckich@gmail.com>
2022-10-10 20:08:15 +0300

    Chapter 6 - bean scopes

--- chapter-1-spring-core/src/main/java/ioc/App.java
+++ chapter-1-spring-core/src/main/java/ioc/App.java
@@ -1,17 +1,19 @@
 package ioc;
 
-import ioc.coaches.CricketCoach;
+import ioc.coaches.Coach;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext.xml")) {
-            CricketCoach coach = context.getBean("cCoach", CricketCoach.class);
-            System.out.println(coach.getDailyWorkout());
-            System.out.println(coach.getDailyFortune());
-            System.out.println(coach.getEmailAddress());
-            System.out.println(coach.getTeam());
+                new ClassPathXmlApplicationContext("beanScopeApplicationContext.xml")) {
+            Coach coach1 = context.getBean("singletonCoach", Coach.class);
+            Coach coach2 = context.getBean("singletonCoach", Coach.class);
+            System.out.println("Singleton pointing to the same area of memory: " + (coach1 == coach2));
+
+            coach1 = context.getBean("prototypeCoach", Coach.class);
+            coach2 = context.getBean("prototypeCoach", Coach.class);
+            System.out.println("Prototype pointing to the same area of memory: " + (coach1 == coach2));
         }
     }
 }
