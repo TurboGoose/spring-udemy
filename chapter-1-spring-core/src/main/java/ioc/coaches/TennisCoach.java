78b9f49f9625d28cc306365d01d0ac4eafaaf28e
TurboGoose <antonpuckich@gmail.com>
2022-10-12 10:28:37 +0300

    Chapter 6 - configuration with java annotations (DI via field)

--- chapter-1-spring-core/src/main/java/ioc/coaches/TennisCoach.java
+++ chapter-1-spring-core/src/main/java/ioc/coaches/TennisCoach.java
@@ -6,16 +6,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+    @Autowired
     private FortuneService fortuneService;
 
     public TennisCoach() {
         System.out.println("Call default constructor");
-    }
-
-    @Autowired
-    public void setFortuneService(FortuneService fortuneService) {
-        System.out.println("Call setter with " + fortuneService);
-        this.fortuneService = fortuneService;
     }
 
     @Override
